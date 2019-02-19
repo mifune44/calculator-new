9ff6bc66f1bd5fbcd5ec1f1f10136910e439586d
Joshua Landry <46336653+mifune44@users.noreply.github.com>
2019-02-18 20:34:31 -0800

    Add files via upload

---
+++ calc new/js/scripts.js
@@ -0,0 +1,58 @@
+//
+// var add = function(number1, number2) {
+//   return number1 + number2;
+// };
+//
+// var number1 = parseInt(prompt("Enter a number:"));
+// var number2 = parseInt(prompt("Enter another number:"));
+// var result = add(number1, number2);
+// alert(result);
+//
+// var subtract = function(number1, number2) {
+//   return number1 - number2;
+// };
+//
+// var number1 = parseInt(prompt("Enter a number:"));
+// var number2 = parseInt(prompt("Enter another number:"));
+// var result = subtract(number1, number2);
+// alert(result);
+//
+// var multiply = function(number1, number2) {
+//   return number1 * number2;
+// };
+//
+// var number1 = parseInt(prompt("Enter a number:"));
+// var number2 = parseInt(prompt("Enter another number:"));
+// var result = multiply(number1, number2);
+// alert(result);
+//
+// var divide = function(number1, number2) {
+//   return number1 / number2;
+// };
+//
+// var number1 = parseInt(prompt("Enter a number:"));
+// var number2 = parseInt(prompt("Enter another number:"));
+// var result = divide(number1, number2);
+// alert(result);
+
+
+// //bmi
+// var bmi = function(height,weight){
+//   return 703 * (weight/(height*height));
+// };
+//
+// var height = parseInt(prompt("Enter your height (in inches):"));
+// var weight = parseInt(prompt("Enter your weight (in lbs):"));
+// var result = bmi(height,weight);
+// alert(result);
+
+//c to f
+// var ctof = function(celsius){
+//   return celsius * 9 / 5 + 32;
+// };
+//
+// var celsius = parseInt(prompt("Enter your temperature (in celsius):"));
+// var result = ctof(celsius);
+// alert(result);
+
+var sample_regex = /epicodus/gi
